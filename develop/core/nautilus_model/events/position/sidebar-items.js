a3c2e707018850ca7e6024feb3dbe3ddb51f41b6
cjdsellers <chris@cjdsellers.io>
2023-07-03 02:25:28 +0000

    Update gh-pages to output generated at bd92ad6

--- develop/core/nautilus_model/events/position/sidebar-items.js
+++ develop/core/nautilus_model/events/position/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["PositionEvent"],"struct":["PositionChanged","PositionClosed","PositionOpened"]};
+window.SIDEBAR_ITEMS = {"enum":["PositionEvent"],"struct":["PositionChanged","PositionClosed","PositionOpened","PositionState"]};
